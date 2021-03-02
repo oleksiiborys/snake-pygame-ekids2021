a68063b50172c17fae8591da22796831a66d2cbf
Oleksii_Borys <Lopata_1>
2021-03-02 19:00:10 +0200

    Quit

--- snake.py
+++ snake.py
@@ -7,7 +7,8 @@ pygame.display.set_caption('Snake game by Edureka')
 game_over = False
 while not game_over:
     for event in pygame.event.get():
-        print(event)  # prints out all the actions that take place on the screen
+        if event.type == pygame.QUIT:
+            game_over = True
 
 pygame.quit()
 quit()
