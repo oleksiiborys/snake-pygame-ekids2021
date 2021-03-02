9aa36f622846b5cb5e6e3e935cd275262c9b07db
Oleksii_Borys <Lopata_1>
2021-03-02 21:43:42 +0200

    1. Rectangle 2. Printing all events 3. Quit

--- snake.py
+++ snake.py
@@ -8,6 +8,8 @@ game_over = False
 while not game_over:
     for event in pygame.event.get():
         print(event) #prints out all the actions that take place on the screen
+        if event.type==pygame.QUIT:
+            game_over=True
 
 pygame.quit()
 quit()
