57acbde4af73d2cbb8daf7f1413de2bfb13f2f67
Oleksii_Borys <Lopata_1>
2021-03-02 21:57:43 +0200

    1. Rectangle 2. Printing all events 3. Quit 4. Simple snake - one rectangle 5. Moving the snake

--- snake.py
+++ snake.py
@@ -1,22 +1,51 @@
 import pygame
 
-pygame.init()
-dis = pygame.display.set_mode(size=(400, 300))
+white = (255,255,255)
+black = (0,0,0)
+blue = (0, 0, 255)
+light_blue = (96, 148, 188)
+red = (255, 0, 0)
 
+pygame.init()
+
+dis = pygame.display.set_mode(size=(800, 600))
 pygame.display.set_caption("Snake game for EKIDS2021")
 
-blue = (0, 0, 255)
-red = (255, 0, 0)
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
 
 game_over = False
 while not game_over:
     for event in pygame.event.get():
-        print(event)  # prints out all the actions that take place on the screen
+        # print(event)  # prints out all the actions that take place on the screen
         if event.type == pygame.QUIT:
             game_over = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
 
-        pygame.draw.rect(dis, blue, [200, 150, 10, 10])
-        pygame.display.update()
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(light_blue)
+    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+
+    pygame.display.update()
+    clock.tick(30)
 
 pygame.quit()
 quit()
