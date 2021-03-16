a85c7761d06148515352a762dafa1e983882c792
Oleksii_Borys <Lopata_1>
2021-03-16 22:51:49 +0200

    Lesson2 preparations

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # snake-pygame-ekids2021
+Based on https://www.edureka.co/blog/snake-game-with-pygame/
--- snake.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-pygame.init()
-dis = pygame.display.set_mode(size=(400, 300))
-pygame.display.update()
-pygame.quit()
-quit()
