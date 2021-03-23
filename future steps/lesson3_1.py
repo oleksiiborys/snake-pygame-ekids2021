dd42d15d3398d3f385309f33a95b8ff90953fce8
Oleksii_Borys <Lopata_1>
2021-03-23 23:45:37 +0200

    Lesson3 preparations

--- future steps/lesson3_1.py
+++ future steps/lesson3_1.py
@@ -1,37 +1,38 @@
 import pygame
 import time
 
-white = (255,255,255)
-black = (0,0,0)
+white = (255, 255, 255)
+black = (0, 0, 0)
 blue = (0, 0, 255)
 light_blue = (96, 148, 188)
 red = (255, 0, 0)
 
 dis_width = 800
 dis_height = 600
-
 pygame.init()
 dis = pygame.display.set_mode(size=(800, 600))
 pygame.display.set_caption("Snake game for EKIDS2021")
 
-game_over = False
+x1 = dis_width / 2
+y1 = dis_height / 2
 
-x1 = dis_width/2
-y1 = dis_height/2
-
-snake_block=10
+snake_block = 10
+snake_speed = 30
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
-snake_speed=30
 
 font_style = pygame.font.SysFont(None, 50)
 
-def message(msg,color):
+game_over = False
+
+
+def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width/2, dis_height/2])
+    dis.blit(mesg, [dis_width / 2, dis_height / 2])
+
 
 while not game_over:
     for event in pygame.event.get():
@@ -63,7 +64,7 @@ while not game_over:
 
     clock.tick(snake_speed)
 
-message("You lost",red)
+message("You lost", red)
 pygame.display.update()
 time.sleep(2)
 
