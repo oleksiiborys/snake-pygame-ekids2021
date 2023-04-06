dd8f83780b62dc69d1e3bc8a3257890d03da7749
Oleksii Borys HsCon <oleksii.borys.con@hotstar.com>
2023-04-06 22:56:34 +0300

    added samples for arrays

---
+++ examples/arrays.py
@@ -0,0 +1,41 @@
+print("-0-----------")
+
+array1 = (1,3,5)
+# array1[1] = 7 # не_змінні значення
+print(array1)
+
+print("-1-----------")
+
+array2 = [1,3,5]
+
+array2.insert(1, 2)
+array2.insert(3, 4)
+array2.append(6)
+
+print(array2)
+array2[2] = "0" # змінні значення
+print(array2)
+
+
+print("-2-----------")
+
+alphabet = ["a", "b", "c", "d", "e"]
+for letter in alphabet:
+    print(letter)
+
+print("-3-----------")
+alphabet_length = alphabet.__len__()
+i = 0
+while i < alphabet_length:
+    print(alphabet[i])
+    i +=1
+
+print("-4-----------")
+
+alphabet2 = [[1, "a"], [2, "b"], [3, "c"], [4, "d"], [5, "e"]]
+
+for pair in alphabet2:
+    print(pair)
+    print(pair[0].__str__() + " - " + pair[1])
+
+print("-the-end-----")
